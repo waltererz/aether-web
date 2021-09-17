55385b1bd47c48d0df97bba9c5321c549a9ddce3
waltererz <walter.erzsamatory@gmail.com>
2021-09-17 10:00:22 +0000

    서버 이전 완료

--- aether-web/webpack.mix.js
+++ aether-web/webpack.mix.js
@@ -11,16 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.options({ runtimeChunkPath: './js/extract' });
+mix.setPublicPath('./build').options({ runtimeChunkPath: './js/common' });
 
 mix.js('resources/js/index.jsx', './js/app.js')
    .js('resources/js/landing.jsx', './js/landing.js')
    .js('resources/js/admin.jsx', './admin/js/app.js')
    .react().version();
 
-mix.extract(['react', 'react-dom', 'react-redux', 'react-router-dom', 'redux', 'axios', 'universal-cookie'], './js/extract/chunk.1.js')
-   .extract(['@material-ui/core', '@material-ui/icons', '@material-ui/styles', '@emotion/react', '@emotion/styled'], './js/extract/chunk.2.js')
-   .extract(['@popperjs/core', '@ckeditor/ckeditor5-build-classic', '@ckeditor/ckeditor5-react'], './js/extract/chunk.3.js')
+mix.extract(['react', 'react-dom', 'react-redux', 'react-router-dom', 'redux', 'axios', 'universal-cookie'], './js/common/chunk.1.js')
+   .extract(['@material-ui/core', '@material-ui/icons', '@material-ui/styles', '@emotion/react', '@emotion/styled'], './js/common/chunk.2.js')
+   .extract(['@popperjs/core', '@ckeditor/ckeditor5-build-classic', '@ckeditor/ckeditor5-react'], './js/common/chunk.3.js')
    .extract({
-      to: './js/extract/chunk.a.js'
+      to: './js/common/chunk.a.js'
    });
